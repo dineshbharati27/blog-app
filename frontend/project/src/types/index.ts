24d8ca97b90a56deccf2ae0d440298308962c76c
dinesh bharati <dineshbharati705@gmail.com>
2025-02-02 19:24:50 +0530

    changing the backend link

---
+++ frontend/project/src/types/index.ts
@@ -0,0 +1,31 @@
+// export interface User {
+//   id: string;
+//   name: string;
+//   email: string;
+//   bio: string;
+//   avatar?: string;
+// }
+
+// export interface Blog {
+//   id: string;
+//   title: string;
+//   description: string;
+//   imageUrl: string;
+//   categories: string[];
+//   author: User;
+//   createdAt: string;
+// }
+
+// export interface AuthState {
+//   user: User | null;
+//   token: string | null;
+//   isLoading: boolean;
+//   error: string | null;
+// }
+
+// export interface BlogState {
+//   blogs: Blog[];
+//   userBlogs: Blog[];
+//   isLoading: boolean;
+//   error: string | null;
+// }
